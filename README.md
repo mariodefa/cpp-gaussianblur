d7be4923b534095b553d79ea9ab2f9075f14fdc5
Mario Rojo <marioruiz.rojo@defa.com>
2024-06-11 10:32:08 +0200

    initial

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # cpp-gaussianblur
 Image Gaussian blur processor C++ example using OpenCV2
+
+## Create Docker Imgs
+```Shell
+cd amarrettopencv
+docker build -t amarrettopencv .
+cd ..
+docker build -t gaussianblur .
+```  
+## Run it
+```Shell
+./build/GaussianBlur
+```
---
+++ gaussianBlur.cpp
@@ -0,0 +1,17 @@
+#include <opencv2/opencv.hpp>
+#include <iostream>
+
+int main() {
+    cv::Mat image = cv::imread("input.jpg", cv::IMREAD_COLOR);
+    if(image.empty()) {
+        std::cerr << "Could not open or find the image" << std::endl;
+        return -1;
+    }
+
+    cv::Mat processedImage;
+    cv::GaussianBlur(image, processedImage, cv::Size(15, 15), 0);
+
+    cv::imwrite("output.jpg", processedImage);
+
+    return 0;
+}
